7311c76dc16eed21988586d91fb8578183d19dee
kashishdipro <kamanashishdatta@gmail.com>
2023-12-27 18:55:55 +0600

    posting message to db

--- src/Pages/Contact/Message/Message.js
+++ src/Pages/Contact/Message/Message.js
@@ -1,6 +1,41 @@
 import React from 'react';
 
 const Message = () => {
+    // Handle Client Message From Input Field
+    const handleMessage = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const clientName = form.clientName.value;
+        const email = form.email.value;
+        const subject = form.subject.value;
+        const phoneNumber = form.phoneNumber.value;
+        
+        // Message Package
+        const message = {
+            clientName,
+            email,
+            subject,
+            phoneNumber
+        }
+
+        // Post to Message to DB
+        fetch('http://localhost:5000/messages', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(message)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.acknowledged){
+                alert('Message send successfully');
+                form.reset();
+            }
+        })
+        .catch(err => console.error(err))
+    }
+
     return (
         <div className="hero min-h-screen bg-cyan-100">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -9,33 +44,33 @@ const Message = () => {
                 <p className="text-black/70 py-6">Call me if needed on 01710444700 or simply contact me below.</p>
                 </div>
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-cyan-200">
-                <form className="card-body">
+                <form onSubmit={handleMessage} className="card-body">
                     <div className="form-control">
                     <label className="label">
                         <span className="label-text text-black/70">Name</span>
                     </label>
-                    <input type="name" placeholder="Your name here" className="input input-bordered" required />
+                    <input name='clientName' type="name" placeholder="Your name here" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                     <label className="label">
                         <span className="label-text text-black/70">Email</span>
                     </label>
-                    <input type="email" placeholder="email" className="input input-bordered" required />
+                    <input name='email' type="email" placeholder="email" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                     <label className="label">
                         <span className="label-text text-black/70">Subject</span>
                     </label>
-                    <input type="subject" placeholder="Your subject here" className="input input-bordered" required />
+                    <input name='subject' type="subject" placeholder="Your subject here" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                     <label className="label">
                         <span className="label-text text-black/70">Mobile Number</span>
                     </label>
-                    <input type="tel" placeholder="Your mobile number" className="input input-bordered" required />
+                    <input name='phoneNumber' type="tel" placeholder="Your mobile number" className="input input-bordered" required />
                     </div>
                     <div className="form-control mt-6">
-                    <button className="btn btn-warning">Send</button>
+                        <input className="btn btn-warning" type='submit' value='Send' input/>
                     </div>
                 </form>
                 </div>
